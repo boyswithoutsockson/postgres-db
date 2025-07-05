b358da12677d99ec044ab29af68045a16a872f8f
Tuomas Poutanen <tuomas.poutanen@gmail.com>
2025-07-05 13:23:50 +0000

    dowload dump also downloads pictures. Also unzips and removes zipfiles

--- download-dump.py
+++ download-dump.py
@@ -6,13 +6,28 @@
 # ///
 
 import gdown
+import zipfile
+import os
 
 def download_file(file_id, output):
     url = f"https://drive.google.com/uc?id={file_id}"
     gdown.download(url, output, quiet=False)
 
+def unzip_file(file_path):
+    with zipfile.ZipFile(file_path, 'r') as z:
+        z.extractall("/".join(file_path.split("/")[:-1]))
+
 if __name__ == "__main__":
-    file_id = "1fCREZR2suqzdY-lBZzQA3Pm-2acUcXaX"
+    file_id = "1cQb23nkz-DAlo33cU96BnPjdnXU9MoFA"
     destination_path = "data/dump.zip"
     download_file(file_id, destination_path)
-    print("done")
+    unzip_file(destination_path)
+    os.remove(destination_path)
+    print(f"done with {destination_path.split("/")[-1]}")
+
+    file_id = "1K0ykFwVEdU-EmwSPC_p6Yx-S4Ko5Bylh"
+    destination_path = "frontend/src/assets/photos-2023-2026.zip"
+    download_file(file_id, destination_path)
+    unzip_file(destination_path)
+    os.remove(destination_path)
+    print(f"done with {destination_path.split("/")[-1]}")
