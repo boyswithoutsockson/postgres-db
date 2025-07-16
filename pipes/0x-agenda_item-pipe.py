2d79376ce9668eee7a570049dd703e9401b9bbbb
Joonas Palosuo <joonas.palosuo@gmail.com>
2025-07-16 09:50:18 +0000

    refactor: transform pipes into preprocess/import clis

--- pipes/0x-agenda_item-pipe.py
+++ pipes/0x-agenda_item-pipe.py
@@ -1,34 +1,55 @@
+import os.path
 import xml.etree.ElementTree as ET
 import csv
 import psycopg2
 
-agenda_item_data = []
+csv_path = 'data/preprocessed/agenda_items.csv'
 
-with open("SaliDBKohta.tsv") as fd:
+def preprocess_data():
+    with open(os.path.join("data", "raw", "SaliDBKohta.tsv")) as fd:
+        agenda_item_data = list(csv.reader(fd, delimiter="\t", quotechar='"'))[1:]  # Skip header
 
-    for row in csv.reader(fd, delimiter="\t", quotechar='"'):
-        agenda_item_data.append(row)
+    rows = []
+    for agenda_item in agenda_item_data:
+        rows.append({
+            "id": agenda_item[0],
+            "title": agenda_item[9],
+            "start_time": agenda_item[16],
+            "session": f"{agenda_item[1]}",
+            "sequence": agenda_item[6],
+            "number": agenda_item[7]
+        })
 
-conn = psycopg2.connect(database="postgres",
-                        host="db",
-                        user="postgres",
-                        password="postgres",
-                        port="5432")
-cursor = conn.cursor()
+    with open(csv_path, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=["id", "title", "start_time", "session", "sequence", "number"])
+        writer.writeheader()
+        writer.writerows(rows)
 
-agenda_item_data = agenda_item_data[1:]
-for agenda_item in agenda_item_data:
+def import_data():
+    conn = psycopg2.connect(database="postgres",
+                            host="db",
+                            user="postgres",
+                            password="postgres",
+                            port="5432")
+    cursor = conn.cursor()
 
-    id = agenda_item[0]
-    title = agenda_item[9]
-    start_time = agenda_item[16]
-    session = f"{agenda_item[1]}"
-    sequence = agenda_item[6]
-    number = agenda_item[7]
+    with open(csv_path) as f:
+        cursor.copy_expert("COPY agenda_items(id, title, start_time, session, sequence, number) FROM stdin DELIMITERS ',' CSV HEADER QUOTE '\"';", f)
 
-    cursor.execute("INSERT INTO agenda_items (id, title, start_time, session, sequence, number) VALUES (%s, %s, %s, %s, %s, %s);", 
-                    (id, title, start_time, session, sequence, number))
-    
-conn.commit()
-cursor.close()
-conn.close()
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--preprocess-data", help="preprocess the data", action="store_true")
+    parser.add_argument("--import-data", help="import preprocessed data", action="store_true")
+    args = parser.parse_args()
+    if args.preprocess_data:
+        preprocess_data()
+    if args.import_data:
+        import_data()
+    if not args.preprocess_data and not args.import_data:
+        preprocess_data()
+        import_data()
--- pipes/vote_pipe2.py
+++ pipes/vote_pipe2.py
@@ -2,8 +2,8 @@ import os.path
 import csv
 import psycopg2
 import pandas as pd
-from db import pg_engine
 
+csv_path = 'data/preprocessed/votes.csv'
 
 vote_dict = {"Jaa": "yes",
             "Ja": "yes",
@@ -18,34 +18,42 @@ vote_dict = {"Jaa": "yes",
             "Avstår": "abstain",
             "Blank": "abstain"}
 
-def vote_pipe():
-    with open(os.path.join("data", "SaliDBAanestysEdustaja.tsv")) as f:
-        print("Reading file...")
+def preprocess_data():
+    with open(os.path.join("data", "raw", "SaliDBAanestysEdustaja.tsv")) as f:
         vote_data = pd.read_csv(f, sep="\t")[["EdustajaHenkiloNumero", "AanestysId", "EdustajaAanestys"]]
-        print("Done!")
 
     vote_data["EdustajaAanestys"] = vote_data["EdustajaAanestys"].str.strip().apply(
         lambda x: vote_dict[x]
     )
 
-    print("Writing csv...")
     vote_data.columns = ["mp_id", "ballot_id", "vote"]
-    vote_data.to_csv("data/votes.csv", index=False, header=False)
-    print("Done!")
+    vote_data.to_csv(csv_path, index=False)
 
-
-    print("Writing database...")
+def import_data():
     conn = psycopg2.connect(database="postgres",
                             host="db",
                             user="postgres",
                             password="postgres",
                             port="5432")
     cursor = conn.cursor()
-    with open('data/votes.csv') as f:
-        cursor.copy_from(f, 'votes', columns=('mp_id', 'ballot_id', 'vote'), sep=",")
-
-    print("Done!")
-
+    with open(csv_path) as f:
+        cursor.execute("ALTER TABLE votes DISABLE TRIGGER ALL;")
+        cursor.copy_expert("COPY votes(mp_id, ballot_id, vote) FROM stdin DELIMITERS ',' CSV HEADER QUOTE '\"';", f)
+        cursor.execute("ALTER TABLE votes ENABLE TRIGGER ALL;")
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 if __name__ == '__main__':
-    vote_pipe()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--preprocess-data", help="preprocess the data", action="store_true")
+    parser.add_argument("--import-data", help="import preprocessed data", action="store_true")
+    args = parser.parse_args()
+    if args.preprocess_data:
+        preprocess_data()
+    if args.import_data:
+        import_data()
+    if not args.preprocess_data and not args.import_data:
+        preprocess_data()
+        import_data()
